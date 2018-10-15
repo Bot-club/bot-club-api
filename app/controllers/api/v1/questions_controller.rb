9ed2aa66232ef78d1a7d8d2cae3b421b339b0224
Vignesh Suresh <pingviky@gmail.com>
2018-10-15 11:48:34 +0530

    Implement Question and Answer API

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,13 +1,24 @@
 class Api::V1::QuestionsController < ApplicationController
 
      # POST /create
-    def create
+    def create_question
         @question = Question.new(user: @user, title: params['title'])
         if @question.save
             response = { message: 'Question submitted successfully'}
             render json: response, status: :created 
         else
             render json: @question.errors, status: :bad
+        end 
+    end
+
+    # POST /create
+    def create_answer
+        @answer = Question.new(user: @user, description: params['description'], question_id: params['question_id'])
+        if @answer.save
+            response = { message: 'Answer submitted successfully'}
+            render json: response, status: :created 
+        else
+            render json: @answer.errors, status: :bad
         end 
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
       post 'user/register'            => 'users#register'
       post 'user/sign_in'             => 'users#sign_in'
       delete 'user/sign_out'          => 'users#sign_out'
+
+      # Question
+      post 'question/create_question'            => 'questions#create_question'
+      post 'question/create_answer'              => 'questions#create_answer'
     end
   end
 end
